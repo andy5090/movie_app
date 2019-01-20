3a5e40c1b07c00b77b150ad4d46a1c9b8212788c
Andy Lee <andy5090@naver.com>
2019-01-21 01:34:38 +0900

    Updates

---
+++ precache-manifest.0252c886062cbf0866b54ff9ef431034.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "372a5ca1905b4fdaaabb",
+    "url": "/movie_app/static/js/main.372a5ca1.chunk.js"
+  },
+  {
+    "revision": "d120fdf0ddba1865b329",
+    "url": "/movie_app/static/js/1.d120fdf0.chunk.js"
+  },
+  {
+    "revision": "372a5ca1905b4fdaaabb",
+    "url": "/movie_app/static/css/main.52e72322.chunk.css"
+  },
+  {
+    "revision": "611816d99d534c53c4338469a716c752",
+    "url": "/movie_app/index.html"
+  }
+];
